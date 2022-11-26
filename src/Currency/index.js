d66a7f71576ce755e7b7c8633c21ac06d04d2442
Adrian Jaroszuk <adrianjaroszuk571@gmail.com>
2022-11-26 14:16:41 +0100

    Added currency component

---
+++ src/Currency/index.js
@@ -0,0 +1,21 @@
+export const currencies = [
+    {
+        id: 1,
+        short: "USD",
+        name: "Dolar Amerykański",
+        rate: 4.5480,
+    },
+    {
+        id: 2,
+        short: "EUR",
+        name: "EURO",
+        rate: 4.6958,
+    },
+    {
+        id: 3,
+        short: "GBP",
+        name: "Funt Brytyjski",
+        rate: 5.4205,
+    }
+];
+
