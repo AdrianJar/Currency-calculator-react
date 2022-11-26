de4b55667ffd56e94d88b9e3619535867c0271e6
Adrian Jaroszuk <adrianjaroszuk571@gmail.com>
2022-11-26 10:46:01 +0100

    Added calculator to  react.app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <body class="body">
+      <form class="form js-form">
+        <fieldset class="form__fieldset">
+          <legend class="form__legend">Kalkulator walutowy</legend>
+          <p>
+            <label>
+              <span class="form__labelText"> Kwota w zł: </span>
+              <input
+                placeholder="Wpisz kwotę w zł"
+                class="form__field js-amountInPLN"
+                type="number"
+                min="0"
+                required
+                step="0.01"
+              />
+            </label>
+          </p>
+          <p>
+            <label>
+              <span class="form__labelText"> Wybierz walutę: </span>
+              <select class="form__field js-currencyName">
+                <option value="USD">Dolar Amerykański</option>
+                <option value="EUR">Euro</option>
+                <option value="GBP">Funt Brytyjski</option>
+              </select>
+            </label>
+          </p>
+          <p class="form__calculationResult">
+            Wynik przeliczenia: <strong class="js-result"></strong>
+          </p>
+          <p>
+            <button class="form__button">Przelicz</button>
+          </p>
+          <p>Kurs wymiany wg NBP z dnia 2022-11-18</p>
+        </fieldset>
+      </form>
+    </body>
   );
 }
 
