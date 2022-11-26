fc465bc6b9ea6163f42d42e9f5fd05a0efe12c83
Adrian Jaroszuk <adrianjaroszuk571@gmail.com>
2022-11-26 17:55:45 +0100

    Added functions calculate exchanged amount

--- src/App.js
+++ src/App.js
@@ -2,14 +2,28 @@ import Header from "./Header";
 import Footer from "./Footer";
 import Form from "./Form";
 import Container from "./Container";
+import { currencies } from "./Currency"
+import { useState } from "react";
 
 function App() {
+  const [result, getResult] = useState();
+
+  const calculatedResult = (exchangeAmount, currency) => {
+    const exchangeRate = currencies.find(({ short }) => short === currency).rate;
+    const short = currencies.find(({ short }) => short === currency).short;
+
+    getResult((exchangeAmount / exchangeRate).toFixed(2) + short);
+  };
+
   return (
     <Container>
       <Header
         title="Kalklator walut"
       />
-      <Form />
+      <Form
+        calculatedResult={calculatedResult}
+        result={result}
+      />
       <Footer
         title="Kalukulator walut by Adrian Jaroszuk"
       />
--- src/Form/index.js
+++ src/Form/index.js
@@ -1,40 +1,78 @@
+import { useState } from "react";
+import { currencies } from "../Currency/index"
 
+const Form = ({ result, calculatedResult, getResult }) => {
 
-const Form = () => (
-    <form class="form js-form">
-        <fieldset class="form__fieldset">
-            <legend class="form__legend">Kalkulator walutowy</legend>
-            <p>
-                <label>
-                    <span class="form__labelText"> Kwota w zł: </span>
-                    <input
-                        placeholder="Wpisz kwotę w zł"
-                        class="form__field js-amountInPLN"
-                        type="number"
-                        min="0"
-                        required
-                        step="0.01"
-                    />
-                </label>
-            </p>
-            <p>
-                <label>
-                    <span class="form__labelText"> Wybierz walutę: </span>
-                    <select class="form__field js-currencyName">
-                        <option value="USD">Dolar Amerykański</option>
-                        <option value="EUR">Euro</option>
-                        <option value="GBP">Funt Brytyjski</option>
-                    </select>
-                </label>
-            </p>
-            <p class="form__calculationResult">
-                Wynik przeliczenia: <strong class="js-result"></strong>
-            </p>
-            <p>
-                <button class="form__button">Przelicz</button>
-            </p>
-        </fieldset>
-    </form>
-);
+    const [exchangeAmount, setExchangeAmount] = useState("");
 
-export default Form;
+    const [currency, setCurrency] = useState("USD");
+
+    const onFormSubmit = (event) => {
+        event.preventDefault();
+        calculatedResult(exchangeAmount, currency);
+    };
+
+    const onFormReset = () =>{
+        setExchangeAmount("")
+        getResult("")
+    };
+
+    return (
+        <form
+            onSubmit={onFormSubmit}
+            onReset={onFormReset}
+        >
+            <fieldset>
+                <legend>Kalkulator walutowy</legend>
+                <div>
+                    <label>
+                        <span>
+                            Kwota w zł:
+                        </span>
+                        <input
+                            value={exchangeAmount}
+                            onChange={({ target }) => setExchangeAmount(target.value)}
+                            placeholder="Wpisz kwotę w zł"
+                            type="number"
+                            min="0.01"
+                            step="0.01"
+                            required
+                        />
+                    </label>
+                </div>
+                <div>
+                    <label>
+                        Wybierz walutę:
+                        <select
+                            name="currency"
+                            value={currency}
+                            onChange={({ target }) => setCurrency(target.value)}
+                        >
+                            {currencies.map(currency => (
+                                <option
+                                    key={currency.id}
+                                    value={currency.short}
+                                >
+                                    {currency.name}
+                                </option>
+                            ))};
+                        </select>
+                    </label>
+                </div>
+                <div>
+                    Wynik przeliczenia:{result}
+                </div>
+                <div>
+                    <button>Przelicz</button>
+                    <button
+                    type="reset"
+                    >
+                        Reset
+                        </button>
+                </div>
+            </fieldset>
+        </form>
+    )
+    };
+
+    export default Form;
