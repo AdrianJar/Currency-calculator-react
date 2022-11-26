b44e6dbb62fd1d2519c55bcd98f2f1d83f4b0dfd
Adrian Jaroszuk <adrianjaroszuk571@gmail.com>
2022-11-26 14:17:38 +0100

    Added container component to app.js

--- src/App.js
+++ src/App.js
@@ -1,50 +1,19 @@
 import Header from "./Header";
 import Footer from "./Footer";
+import Form from "./Form";
+import Container from "./Container";
 
 function App() {
   return (
-    <body class="body">
+    <Container>
       <Header
         title="Kalklator walut"
       />
-      <form class="form js-form">
-        <fieldset class="form__fieldset">
-          <legend class="form__legend">Kalkulator walutowy</legend>
-          <p>
-            <label>
-              <span class="form__labelText"> Kwota w zł: </span>
-              <input
-                placeholder="Wpisz kwotę w zł"
-                class="form__field js-amountInPLN"
-                type="number"
-                min="0"
-                required
-                step="0.01"
-              />
-            </label>
-          </p>
-          <p>
-            <label>
-              <span class="form__labelText"> Wybierz walutę: </span>
-              <select class="form__field js-currencyName">
-                <option value="USD">Dolar Amerykański</option>
-                <option value="EUR">Euro</option>
-                <option value="GBP">Funt Brytyjski</option>
-              </select>
-            </label>
-          </p>
-          <p class="form__calculationResult">
-            Wynik przeliczenia: <strong class="js-result"></strong>
-          </p>
-          <p>
-            <button class="form__button">Przelicz</button>
-          </p>
-        </fieldset>
-      </form>
+      <Form />
       <Footer
-            title="Kalukulator walut by Adrian Jaroszuk"
-          />
-    </body>
+        title="Kalukulator walut by Adrian Jaroszuk"
+      />
+    </Container>
   );
 }
 
