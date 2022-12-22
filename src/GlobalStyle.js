7118d18d4e00840960ec4c14328b32ddccfc58ce
Adrian Jaroszuk <adrianjaroszuk571@gmail.com>
2022-12-22 11:39:40 +0100

    Added global style component and added it to index

---
+++ src/GlobalStyle.js
@@ -0,0 +1,23 @@
+import { createGlobalStyle } from "styled-components";
+import dolars from "./images/dolars.jpeg"
+
+export const GlobalStyle = createGlobalStyle`
+    html {
+        box-sizing: border-box;
+    }
+
+*, 
+::after,
+::before {
+    box-sizing: inherit;
+}
+
+body {
+    max-width: 1000px;
+    font-family: "Montserrat", sans-serif;
+    padding: 20px;
+    margin: 10px auto;
+    background-image: url("${dolars}");
+}
+
+`;
