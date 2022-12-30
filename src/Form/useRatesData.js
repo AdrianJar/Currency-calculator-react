89ddfd92693e3a3334153e6dcc6074710d96d79f
Adrian Jaroszuk <adrianjaroszuk571@gmail.com>
2022-12-30 22:08:25 +0100

    Added separate hook for api fetch

---
+++ src/Form/useRatesData.js
@@ -0,0 +1,32 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
+
+
+export const useRatesData = () => {
+    const [ratesData, setRatesData] = useState({
+        status: "loading"
+    });
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const exchangeRatesURL = "https://api.exchangerate.host/latest?base=PLN&symbols=USD,EUR,GBP";
+                const response = await axios.get(exchangeRatesURL)
+
+                setRatesData({
+                    date: response.data.date,
+                    rates: response.data.rates,
+                    status: "success"
+                });
+            } catch (error) {
+                console.error(error)
+                setRatesData({
+                    status: "error"
+                });
+            }
+        };
+        setTimeout(fetchData, 1500);
+    }, []);
+
+    return ratesData;
+};
