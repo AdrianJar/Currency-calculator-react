773cc3c62b261e1a8a4ed84bc2900b77eb266fd4
Adrian Jaroszuk <adrianjaroszuk571@gmail.com>
2022-12-31 18:17:11 +0100

    added status check components

--- src/Form/index.js
+++ src/Form/index.js
@@ -11,7 +11,9 @@ import {
     FormResultText,
     FormResultAmount,
     ButtonsContainer,
-    Button
+    Button,
+    Loading,
+    Failure,
 } from "./styled";
 
 const Form = () => {
@@ -39,56 +41,71 @@ const Form = () => {
         <StyledForm
             onSubmit={onFormSubmit}
         >
-            <Fieldset>
-                <div>
-                    <label>
-                        <LabelContent>
-                            Kwota w zł:*
-                        </LabelContent>
-                        <Input
-                            value={amount}
-                            onChange={({ target }) => setAmount(target.value)}
-                            placeholder="Wpisz kwotę w zł"
-                            type="number"
-                            min="0.01"
-                            step="0.01"
-                            required
-                        />
-                    </label>
-                </div>
-                <div>
-                    <label>
-                        <LabelContent>
-                            Wybierz walutę:*
-                        </LabelContent>
-                        <Select
-                            name="currency"
-                            value={currency}
-                            onChange={({ target }) => setCurrency(target.value)}
-                        >
-                            {Object.keys(ratesData.rates).map(currency => (
-                                <option
-                                    key={currency}
+            {ratesData.status === "loading"
+                ? (
+                    <Loading>
+                        Poczekaj chwilę dane są ładowane
+                    </Loading>
+                )
+                :
+                (ratesData.status === "error" ? (
+                    <Failure>
+                        Ups... Coś poszło nie tak...
+                    </Failure>
+                )
+                    :
+                    (<Fieldset>
+                        <div>
+                            <label>
+                                <LabelContent>
+                                    Kwota w zł:*
+                                </LabelContent>
+                                <Input
+                                    value={amount}
+                                    onChange={({ target }) => setAmount(target.value)}
+                                    placeholder="Wpisz kwotę w zł"
+                                    type="number"
+                                    min="0.01"
+                                    step="0.01"
+                                    required
+                                />
+                            </label>
+                        </div>
+                        <div>
+                            <label>
+                                <LabelContent>
+                                    Wybierz walutę:*
+                                </LabelContent>
+                                <Select
+                                    name="currency"
                                     value={currency}
+                                    onChange={({ target }) => setCurrency(target.value)}
                                 >
-                                    {currency}
-                                </option>
-                            ))}
-                        </Select>
-                    </label>
-                </div>
-                <FormResult>
-                    <FormResultText><p>Wynik przewalutowania:</p></FormResultText>
-                    <FormResultAmount>{result}</FormResultAmount>
-                </FormResult>
-                <ButtonsContainer>
-                    <Button>Przelicz</Button>
-                    <Button type="reset">Reset</Button>
-                </ButtonsContainer>
-                <Footer
-                    title="Pola oznaczone * są wymagane"
-                />
-            </Fieldset>
+                                    {Object.keys(ratesData.rates).map(currency => (
+                                        <option
+                                            key={currency}
+                                            value={currency}
+                                        >
+                                            {currency}
+                                        </option>
+                                    ))}
+                                </Select>
+                            </label>
+                        </div>
+                        <FormResult>
+                            <FormResultText><p>Wynik przewalutowania:</p></FormResultText>
+                            <FormResultAmount>{result}</FormResultAmount>
+                        </FormResult>
+                        <ButtonsContainer>
+                            <Button>Przelicz</Button>
+                            <Button type="reset">Reset</Button>
+                        </ButtonsContainer>
+                        <Footer
+                            title="Pola oznaczone * są wymagane"
+                        />
+                    </Fieldset>
+                    ))
+            }
         </StyledForm>
     )
 };
