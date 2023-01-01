675f6ce3614dd44fa0a0202a440a86e060ede92d
Adrian Jaroszuk <adrianjaroszuk571@gmail.com>
2023-01-01 21:12:34 +0100

    Edditec code

--- src/Form/LoadAnimation/index.js
+++ src/Form/LoadAnimation/index.js
@@ -2,7 +2,7 @@ import { StyledLoad } from "./styled";
 
 const LoadAnimation = () => (
     <StyledLoad>
-        <div class="lds-ellipsis"><div></div><div></div><div></div><div></div></div>
+        <div className="lds-ellipsis"><div></div><div></div><div></div><div></div></div>
     </StyledLoad>
 );
 
