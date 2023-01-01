fdcd9280fb808a9641f3b4f4760138f89ade1277
Adrian Jaroszuk <adrianjaroszuk571@gmail.com>
2023-01-01 20:55:34 +0100

    Implemented Loading Animation

---
+++ src/Form/LoadAnimation/index.js
@@ -0,0 +1,9 @@
+import { StyledLoad } from "./styled";
+
+const LoadAnimation = () => (
+    <StyledLoad>
+        <div class="lds-ellipsis"><div></div><div></div><div></div><div></div></div>
+    </StyledLoad>
+);
+
+export default LoadAnimation;
