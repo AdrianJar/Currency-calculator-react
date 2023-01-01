b1993e2117d00bcbfdfd956932487e8d3439e5a5
Adrian Jaroszuk <adrianjaroszuk571@gmail.com>
2023-01-01 20:52:15 +0100

    Added new component

---
+++ src/Info/index.js
@@ -0,0 +1,12 @@
+
+const Info = ({ date }) => (
+    <>
+        <p>
+            Kurs pobrany z Europejskiego Banku Centralnego.
+            <br />
+            Kurs aktualny na dzień:<strong>{date}</strong>
+        </p>
+    </>
+);
+
+export default Info
